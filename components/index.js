e9872fca75d01302d7892d84753d548f0d80814c
Daniel Kats <daniel.kats@nortonlifelock.com>
2020-03-05 16:10:34 -0800

    minor fixes

--- components/index.js
+++ components/index.js
@@ -4,6 +4,6 @@ import ReactDOM from "react-dom";
 import MeetingBingo from "./meeting-bingo.jsx";
 
 ReactDOM.render(
-    <MeetingBingo />,
-    document.getElementById("main-container")
+	<MeetingBingo />,
+	document.getElementById("main-container")
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,27 @@
-const webpack = require("webpack");
+// const webpack = require("webpack");
 const path = require("path");
 //const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
-    context: __dirname,
-    entry: "./components/index.js",
-    output: {
-		path: "public/js/dist",
-        filename: "meeting-bingo.bundle.js"
-    },
-    module: {
-        loaders: [
-            // use babel to convert ES6
-            {
-                test: /\.jsx?$/,
-                loader: "babel-loader",
-                exclude: /node_modules/,
-                query: {
-                    presets: ["es2015", "react"]
-                }
-            }
-        ]
-    },
+	entry: "./components/index.js",
+
+	output: {
+		path: path.resolve("public/js/dist"),
+		filename: "meeting-bingo.bundle.js"
+	},
+
+	module: {
+		loaders: [
+			// use babel to convert ES6
+			{
+				test: /\.jsx?$/,
+				loader: "babel-loader",
+				exclude: /node_modules/,
+				query: {
+					presets: ["es2015", "react"]
+				}
+			}
+		]
+	},
 	//plugins: [new BundleAnalyzerPlugin()],
 };
